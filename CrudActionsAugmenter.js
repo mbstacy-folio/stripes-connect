e97ba442b9ced4b4d15ff354160d6a4229bb8b80
Mike Taylor <mike@indexdata.com>
2017-07-10 11:35:34 +0100

    CrudActionsAugmenter now accepts any metadata to augment with

--- CrudActionsAugmenter.js
+++ CrudActionsAugmenter.js
@@ -1,16 +1,16 @@
-// Provides a wrapper for crudActions that inserts `dataKey` into the
-// `meta` object of each action that is created.
+// Provides a wrapper for crudActions that inserts extra metadata into
+// the `meta` object of each action that is created.
 
-function CrudActionsAugmenter(crudActions, dataKey) {
+function CrudActionsAugmenter(crudActions, extraMetadata) {
   return new Proxy(this, {
-    get: (receiver, name) => receiver.augmentWithDataKey.bind(receiver, crudActions, name, dataKey),
+    get: (receiver, name) => receiver.augmentWithMetadata.bind(receiver, crudActions, name, extraMetadata),
   });
 }
 
-CrudActionsAugmenter.prototype.augmentWithDataKey = (crudActions, name, dataKey, arg1, arg2, arg3) => {
+CrudActionsAugmenter.prototype.augmentWithMetadata = (crudActions, name, extraMetadata, arg1, arg2, arg3) => {
   if (!crudActions[name]) console.log(`CrudActionsAugmenter: no such function '${name}' in`, crudActions);
   const x = crudActions[name](arg1, arg2, arg3);
-  return Object.assign({}, x, { meta: Object.assign({}, x.meta, { dataKey }) });
+  return Object.assign({}, x, { meta: Object.assign({}, x.meta, extraMetadata) });
 };
 
 export default CrudActionsAugmenter;
